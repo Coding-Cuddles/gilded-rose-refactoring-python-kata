3d4a6d2a2763022e776e24d05d35676136ee1776
Yury Bayda <yury@bayda.pro>
2023-04-05 23:23:53 -0700

    Refactor tests to use fixture

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,101 +1,88 @@
-from gilded_rose import GildedRose, Item
+import pytest
+
+from gilded_rose import GildedRose
 
 
-def test_standard_item_quality_decreases_by_one_each_day():
-    items = [Item(name="Milk", sell_in=5, quality=10)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 9
+@pytest.fixture
+def inn():
+    return GildedRose()
 
 
-def test_standard_item_quality_decreases_twice_as_fast_after_sell_by_date():
-    items = [Item(name="Milk", sell_in=0, quality=3)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 1
+def test_standard_item_quality_decreases_by_one_each_day(inn):
+    inn.add_item("Milk", 5, 10)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 9
 
 
-def test_standard_item_quality_is_never_negative():
-    items = [Item(name="Milk", sell_in=0, quality=0)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 0
+def test_standard_item_quality_decreases_twice_as_fast_after_sell_by_date(inn):
+    inn.add_item("Milk", 0, 3)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 1
 
 
-def test_aged_brie_quality_increases_by_one_each_day():
-    items = [Item(name="Aged Brie", sell_in=10, quality=10)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 11
+def test_standard_item_quality_is_never_negative(inn):
+    inn.add_item("Milk", 0, 0)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 0
 
 
-def test_aged_brie_quality_is_never_above_50():
-    items = [Item(name="Aged Brie", sell_in=8, quality=50)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 50
+def test_aged_brie_quality_increases_by_one_each_day(inn):
+    inn.add_item("Aged Brie", 10, 10)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 11
 
 
-def test_backstage_passes_quality_increases_by_one_each_day():
-    items = [
-        Item(name="Backstage passes to a TAFKAL80ETC concert",
-             sell_in=15,
-             quality=20)
-    ]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 21
+def test_aged_brie_quality_is_never_above_50(inn):
+    inn.add_item("Aged Brie", 8, 50)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 50
 
 
-def test_backstage_passes_quality_increases_by_2_when_sell_in_10_or_less():
-    items = [
-        Item(name="Backstage passes to a TAFKAL80ETC concert",
-             sell_in=10,
-             quality=24)
-    ]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 26
+def test_backstage_passes_quality_increases_by_one_each_day(inn):
+    inn.add_item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 21
 
 
-def test_backstage_passes_quality_increases_by_3_when_sell_in_5_or_less():
-    items = [
-        Item(name="Backstage passes to a TAFKAL80ETC concert",
-             sell_in=5,
-             quality=33)
-    ]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 36
+def test_backstage_passes_quality_increases_by_2_when_sell_in_10_or_less(inn):
+    inn.add_item("Backstage passes to a TAFKAL80ETC concert", 10, 24)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 26
 
 
-def test_backstage_passes_quality_is_zero_after_sell_in_date():
-    items = [
-        Item(name="Backstage passes to a TAFKAL80ETC concert",
-             sell_in=0,
-             quality=45)
-    ]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 0
+def test_backstage_passes_quality_increases_by_3_when_sell_in_5_or_less(inn):
+    inn.add_item("Backstage passes to a TAFKAL80ETC concert", 5, 33)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 36
 
 
-def test_sulfuras_quality_and_sell_in_never_change():
-    items = [Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=80)]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].sell_in == 10
-    assert items[0].quality == 80
+def test_backstage_passes_quality_is_zero_after_sell_in_date(inn):
+    inn.add_item("Backstage passes to a TAFKAL80ETC concert", 0, 45)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 0
 
 
-def test_quality_of_item_never_exceeds_50():
-    items = [
-        Item(name="Aged Brie", sell_in=10, quality=50),
-        Item(name="Backstage passes to a TAFKAL80ETC concert",
-             sell_in=5,
-             quality=49),
-    ]
-    inventory = GildedRose(items)
-    inventory.update_quality()
-    assert items[0].quality == 50
-    assert items[1].quality == 50
+def test_sulfuras_quality_and_sell_in_never_change(inn):
+    inn.add_item("Sulfuras, Hand of Ragnaros", 10, 80)
+    inn.update_quality()
+
+    assert inn.items[0].sell_in == 10
+    assert inn.items[0].quality == 80
+
+
+def test_quality_of_item_never_exceeds_50(inn):
+    inn.add_item("Aged Brie", 10, 50)
+    inn.add_item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+    inn.update_quality()
+
+    assert inn.items[0].quality == 50
+    assert inn.items[1].quality == 50
